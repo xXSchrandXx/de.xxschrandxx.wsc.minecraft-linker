e69b8b58443d77e5c79fcdb895f3fd96f2f3120a
xXSchrandXx <marvinschrand@yahoo.de>
2021-11-19 21:04:23 +0100

    Finish BungeeCord part

--- files/lib/system/minecraft/BungeeMinecraftLinkerHandler.class.php
+++ files/lib/system/minecraft/BungeeMinecraftLinkerHandler.class.php
@@ -2,21 +2,6 @@
 
 namespace wcf\system\minecraft;
 
-class BungeeMinecraftLinkerHandler extends AbstractMinecraftLinkerHandler
+class BungeeMinecraftLinkerHandler extends SpigotMinecraftLinkerHandler
 {
-    /**
-     * @inheritDoc
-     */
-    public function getOnlineMinecraftUsers()
-    {
-        parent::getOnlineMinecraftUsers();
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function sendCode($uuid, $name, $code)
-    {
-        parent::sendCode($uuid, $name, $code);
-    }
 }
