633df5719a4b0dd27f0751c43fc8e27ad5849917
xXSchrandXx <marvinschrand@yahoo.de>
2021-11-23 15:48:31 +0100

    Start readme.md

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Languages: [German](#----german) | [English](#----english)
+<h1>
+    English
+</h1>
+<h1>
+    German
+</h1>
--- make.sh
+++ make.sh
@@ -2,9 +2,6 @@
 PACKAGE_NAME=de.xxschrandxx.wsc.minecraft-linker
 PACKAGE_TYPES=(acptemplates files templates)
 
-rm -rf files/js/*
-tsc --build
-
 for i in "${PACKAGE_TYPES[@]}"
 do
     rm -rf ${i}.tar
@@ -14,10 +11,8 @@ done
 rm -rf ${PACKAGE_NAME}.tar ${PACKAGE_NAME}.tar.gz
 7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.github -x!php_cs.dist -x!phpcs.xml -x!Readme.md -x!pictures
 7z a ${PACKAGE_NAME}.tar.gz ${PACKAGE_NAME}.tar
-rm -rf ${PACKAGE_NAME}.tar
 
 for i in "${PACKAGE_TYPES[@]}"
 do
     rm -rf ${i}.tar
 done
-
