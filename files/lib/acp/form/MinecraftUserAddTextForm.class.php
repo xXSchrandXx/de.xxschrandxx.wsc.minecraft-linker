8ecb55f38bf77680f928aef2705da835ab7236cf
xXSchrandXx <marvinschrand@yahoo.de>
2022-02-08 18:45:57 +0100

    Update MinecraftUserAddTextForm.class.php

--- files/lib/acp/form/MinecraftUserAddTextForm.class.php
+++ files/lib/acp/form/MinecraftUserAddTextForm.class.php
@@ -104,7 +104,7 @@ class MinecraftUserAddTextForm extends AbstractFormBuilderForm
                         ->description('wcf.page.minecraftUserAddACP.minecraftUUID.description')
                         ->minimumLength(36)
                         ->maximumLength(36)
-                        ->pattern('/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i')
+                        ->pattern('^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$')
                         ->placeholder('XXXXXXXX-XXXX-4XXX-XXXX-XXXXXXXXXXXX')
                         ->addValidator(new FormFieldValidator('checkMinecraftUser', function (TextFormField $field) {
                             $minecraftUserList = new MinecraftUserList();
