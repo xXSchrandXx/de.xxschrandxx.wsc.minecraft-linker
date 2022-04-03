bb359af58fb45ab78626298f76a81fa89f7b414b
xXSchrandXx <marvinschrand@yahoo.de>
2022-04-03 20:13:45 +0200

    Add password checker

--- constants.php
+++ constants.php
@@ -5,5 +5,5 @@ define('MINECRAFT_LINKER_IDENTITY', '');
 define('MINECRAFT_MAX_UUIDS', 9);
 define('MINECRAFT_NAME_ENABLED', 1);
 define('MINECRAFT_ENABLE_ACTIVE_USER', 0);
-define('MINECRAFT_COMMAND_LIST', 'list uuids');
-define('MINECRAFT_COMMAND_SENDCODE', 'tellraw {$name} [{"text":"{lang}wcf.minecraft.message{/lang}","clickEvent":{"action":"copy_to_clipboard","value":"{$code}"},"hoverEvent":{"action":"show_text","contents":["{lang}wcf.minecraft.hoverMessage{/lang}"]}}');
+define('MINECRAFT_LINKER_PASSWORD_ENABLED', 1);
+define('MINECRAFT_LINKER_PASSWORD_KEY', '');
---
+++ files/lib/action/MinecraftPasswordCheckAction.class.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace wcf\action;
+
+use wcf\data\user\minecraft\MinecraftUserList;
+use wcf\data\user\User;
+use wcf\system\exception\UserInputException;
+use wcf\util\JSON;
+
+class MinecraftPasswordCheckAction extends AbstractAction
+{
+    /**
+     * @inheritDoc
+     */
+    public $neededModules = ['MINECRAFT_LINKER_PASSWORD_ENABLED','MINECRAFT_LINKER_PASSWORD_KEY'];
+
+    private function getUser(string $uuid): User
+    {
+        $minecraftUserList = new MinecraftUserList();
+        $minecraftUserList->getConditionBuilder()->add('minecraftUUID = ?', [$uuid]);
+        $minecraftUserList->readObjects();
+        $minecraftUsers = $minecraftUserList->getObjects();
+        if (empty($minecraftUsers)) {
+            throw new UserInputException();
+        }
+        return new User(array_values($minecraftUsers)[0]->userID);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute()
+    {
+        parent::execute();
+
+        if (empty($_POST)) {
+             echo JSON::encode([
+                'status' => 'request empty',
+                'statusCode' => 400,
+                'valid' => false
+            ]);
+            return;
+        }
+        if (
+            !array_key_exists('key', $_POST) ||
+            !array_key_exists('uuid', $_POST) ||
+            !array_key_exists('password', $_POST)
+        ) {
+            echo JSON::encode([
+                'status' => 'request missing keys',
+                'statusCode' => 400,
+                'valid' => false
+            ]);
+            return;
+        }
+        $key = $_POST['key'];
+        $password = $_POST['password'];
+        $uuid = $_POST['uuid'];
+        // TODO Check weather post elemts are valid
+        if ($key !== MINECRAFT_LINKER_PASSWORD_KEY) {
+            echo JSON::encode([
+                'status' => 'Unauthorized',
+                'statusCode' => 401,
+                'valid' => false
+            ]);
+            return;
+        }
+        /** @var User */
+        $user = null;
+        try {
+            $user = $this->getUser($uuid);
+        } catch (UserInputException $e) {
+            echo JSON::encode([
+                'status' => $e->getMessage(),
+                'statusCode' => $e->getCode(),
+                'valid' => false
+            ]);
+            return;
+        }
+
+        if ($user->checkPassword($password)) {
+            echo JSON::encode([
+                'status' => 'OK',
+                'statusCode' => 200,
+                'valid' => true
+            ]);
+        } else {
+            echo JSON::encode([
+                'status' => 'OK',
+                'statusCode' => 200,
+                'valid' => false
+            ]);
+        }
+    }
+}
